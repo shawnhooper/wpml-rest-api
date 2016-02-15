6b73b7301ce7f7c512dbf680ccb76b5ebaf92cc9
Shawn Hooper <shawn@actionablebooks.com>
2016-02-15 12:57:02 -0500

    Fix hrefs to translations

--- wpml-rest-api.php
+++ wpml-rest-api.php
@@ -8,9 +8,16 @@ Author: Shawn Hooper
 Author URI: https://profiles.wordpress.org/shooper
 */
 
-add_action( 'rest_api_init', 'wpmlretapi_slug_register_languages' );
+add_action( 'rest_api_init', 'wpmlretapi_init', 1000);
 
-function wpmlretapi_slug_register_languages() {
+function wpmlretapi_init() {
+
+	// Check if WPML is installed
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+	if (!is_plugin_active('sitepress-multilingual-cms/sitepress.php')) {
+		return;
+	}
 
 	// Add WPML fields to all post types
 	// Thanks to Roy Sivan for this trick.
@@ -52,15 +59,28 @@ function wpmlrestapi_register_api_field($post_type) {
 * @return mixed
 */
 function wpmlretapi_slug_get_translations( $object, $field_name, $request ) {
+	global $sitepress;
 	$languages = apply_filters('wpml_active_languages', null);
 	$translations = [];
 
 	foreach ($languages as $language) {
-		$post_id = icl_object_id($object['id'], 'page', false, $language['language_code']);
-		$post = get_post($post_id);
-		if ($post->ID && $object['id'] !== $post->ID) {
-			$translations[] = array('locale' => $language['default_locale'], 'id' => $post->ID, 'post_title' => $post->post_title, 'href' => get_permalink($post->ID));
+		$post_id = wpml_object_id_filter($object['id'], 'post', false, $language['language_code']);
+		if ($post_id === null || $post_id == $object['id']) continue;
+		$thisPost = get_post($post_id);
+
+		$href= apply_filters( 'WPML_filter_link', $language[ 'url' ], $language );
+		if (strpos($href, '?') !== false) {
+			$href = str_replace('?', '/' . $thisPost->post_name . '/?', $href);
+		} else {
+
+			if (substr($href, -1) !== '/') {
+				$href .= '/';
+			}
+
+			$href .= $thisPost->post_name . '/';
 		}
+
+		$translations[] = array('locale' => $language['default_locale'], 'id' => $thisPost->ID, 'post_title' => $thisPost->post_title, 'href' => $href);
 	}
 
 	return $translations;
