4a7a433165701fcd4d93c18615203fe02cb52b24
Shawn Hooper <shawn@fivesense.ca>
2022-10-27 18:16:49 -0400

    Better OOP

--- wpml-rest-api.php
+++ wpml-rest-api.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: WPML REST API
-Version: 1.1.3
+Version: 2.0.0
 Description: Adds links to posts in other languages into the results of a WP REST API query for sites running the WPML plugin.
 Author: Shawn Hooper
 Author URI: https://profiles.wordpress.org/shooper
@@ -10,16 +10,17 @@ Author URI: https://profiles.wordpress.org/shooper
 
 namespace ShawnHooper\WPML;
 
-use WP_Post;
 use WP_REST_Request;
 use RuntimeException;
 
 class WPML_REST_API
 {
 
+    private array $translations = [];
+
     public function wordpress_hooks(): void
     {
-        add_action('rest_api_init', array($this, 'init'), 1000);
+        add_action('rest_api_init', [$this, 'init'], 1000);
     }
 
     public function init(): void
@@ -81,88 +82,8 @@ class WPML_REST_API
     }
 
     /**
-     * Calculate the relative path for this post, supports also nested pages
+     * REST API ENDPOINT Handler
      *
-     * @param WP_Post $thisPost
-     * @return string the relative path for this page e.g. `root-page/child-page`
-     */
-    public function calculate_rel_path(WP_Post $thisPost): string
-    {
-        $post_name = $thisPost->post_name;
-        if ($thisPost->post_parent > 0) {
-            $cur_post = get_post($thisPost->post_parent);
-            if (isset($cur_post)) {
-                $rel_path = $this->calculate_rel_path($cur_post);
-                return $rel_path . "/" . $post_name;
-            }
-        }
-        return $post_name;
-    }
-
-    /**
-     * Retrieve available translations
-     *
-     * @param array $object Details of current post.
-     * @param string $field_name Name of field.
-     * @param WP_REST_Request $request Current request
-     *
-     * @return array
-     * @noinspection PhpUnusedParameterInspection
-     */
-    public function get_translations(array $object, string $field_name, WP_REST_Request $request): array
-    {
-        $languages = apply_filters('wpml_active_languages', null);
-        $translations = [];
-        $show_on_front = get_option('show_on_front');
-        $page_on_front = get_option('page_on_front');
-
-        foreach ($languages as $language) {
-            $post_id = wpml_object_id_filter($object['id'], 'post', false, $language['language_code']);
-            if ($post_id === null || $post_id === $object['id']) {
-                continue;
-            }
-            $thisPost = get_post($post_id);
-
-            // Only show published posts
-            if ('publish' !== $thisPost->post_status) {
-                continue;
-            }
-
-            $href = apply_filters('WPML_filter_link', $language['url'], $language);
-            $href = apply_filters('wpmlrestapi_translations_href', $href, $thisPost);
-            $href = trailingslashit($href);
-
-
-            if (!('page' === $show_on_front && $object['id'] === $page_on_front)) {
-
-                $postUrl = $this->calculate_rel_path($thisPost);
-                if (str_contains($href, '?')) {
-                    $href = str_replace('?', '/' . $postUrl . '/?', $href);
-                } else {
-                    if (!str_ends_with($href, '/')) {
-                        $href .= '/';
-                    }
-
-                    $href .= $postUrl . '/';
-                }
-
-                $translation = [
-                    'locale' => $language['default_locale'],
-                    'id' => $thisPost->ID,
-                    'slug' => $thisPost->post_name,
-                    'post_title' => $thisPost->post_title,
-                    'href' => $href,
-                ];
-
-                $translation = apply_filters('wpmlrestapi_get_translation', $translation, $thisPost, $language);
-                $translations[$language['default_locale']] = $translation;
-            }
-        }
-
-        return $translations;
-    }
-
-    /**
      * Retrieve the current locale
      *
      * @param array $object Details of current post.
@@ -176,12 +97,57 @@ class WPML_REST_API
     public function get_current_locale(array $object, string $field_name, WP_REST_Request $request): string
     {
         $langInfo = wpml_get_language_information($object);
-
         if (is_wp_error($langInfo)) {
             throw new RuntimeException('Unable to retrieve the current locale');
         }
-
         return $langInfo['locale'];
+    }
+
+    /**
+     * REST API ENDPOINT Handler
+     *
+     * Retrieve available translations
+     *
+     * @param array $object Details of current post.
+     * @param string $field_name Name of field.
+     * @param WP_REST_Request $request Current request
+     *
+     * @return array
+     * @noinspection PhpUnusedParameterInspection
+     */
+    public function get_translations(array $object, string $field_name, WP_REST_Request $request): array
+    {
+        $languages = apply_filters('wpml_active_languages', null);
+
+        foreach ($languages as $language) {
+            $this->get_translations_for_language($object, $language);
+        }
+
+        return $this->translations;
+    }
+
+    /**
+     * @param array $object
+     * @param array $language
+     * @return void
+     */
+    private function get_translations_for_language(array $object, array $language) : void {
+        $post_id = wpml_object_id_filter($object['id'], 'post', false, $language['language_code']);
+        if ($post_id === null || $post_id === $object['id']) {
+            return;
+        }
+
+        $thisPost = get_post($post_id);
+
+        $translation = [
+            'locale' => $language['default_locale'],
+            'id' => $thisPost->ID,
+            'slug' => $thisPost->post_name,
+            'post_title' => $thisPost->post_title,
+            'href' => get_permalink($thisPost),
+        ];
+
+        $this->translations[$language['default_locale']] = apply_filters('wpmlrestapi_get_translation', $translation, $thisPost, $language);
     }
 }
 
